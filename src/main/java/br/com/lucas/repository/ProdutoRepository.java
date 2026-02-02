08ce87a8da327dec3cac09a99cfde1da4d99c7a4
lucassantosfontes <fonteslucasdossantos@gmail.com>
2026-02-01 23:57:22 -0300

    criação da interface: ProdutoRepository

---
+++ src/main/java/br/com/lucas/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package br.com.lucas.repository;
+
+import br.com.lucas.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+}
