48fa2b9ecdb7c630403a8079dd0e47acb313f533
lucassantosfontes <fonteslucasdossantos@gmail.com>
2026-02-01 23:38:35 -0300

    criação das Entidades classe: Produto

--- src/main/java/br/com/lucas/entity/Produto.java
+++ src/main/java/br/com/lucas/entity/Produto.java
@@ -1,5 +1,29 @@
 package br.com.lucas.entity;
 
-public class Produto {
+import jakarta.persistence.*;
+import lombok.*;
 
+@Entity
+@Table(name = "produtos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 100)
+	private String nome;
+	
+	private String descricao;
+	
+	@Column(nullable = false)
+	private Double preco;
+	
+	private Integer estoque;
+	
+	
 }
