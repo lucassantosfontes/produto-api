8605b5ba933c83ec9ebe256e5acbff22e5ae4403
lucassantosfontes <fonteslucasdossantos@gmail.com>
2026-02-03 18:22:36 -0300

    criação da classe: UserDetailsServiceImpl

---
+++ src/main/java/br/com/lucas/security/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package br.com.lucas.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import br.com.lucas.repository.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	private UserRepository userRepository;
+	
+	@Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+    }
+	
+
+}
