b3436fcb7219b91297aedbf090ccd84aedf102d9
lucassantosfontes <fonteslucasdossantos@gmail.com>
2026-02-03 18:16:18 -0300

    criação do repositorio: UserRepository

---
+++ src/main/java/br/com/lucas/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package br.com.lucas.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.lucas.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+
+}
