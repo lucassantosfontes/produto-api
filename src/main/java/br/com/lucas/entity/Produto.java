e28a9d3ecb51ae15c5ec646cb46767b50c89d5e9
lucassantosfontes <fonteslucasdossantos@gmail.com>
2026-02-06 15:23:26 -0300

    Get e Set de produtos

--- src/main/java/br/com/lucas/entity/Produto.java
+++ src/main/java/br/com/lucas/entity/Produto.java
@@ -24,6 +24,46 @@ public class Produto {
 	private Double preco;
 	
 	private Integer estoque;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Integer getEstoque() {
+		return estoque;
+	}
+
+	public void setEstoque(Integer estoque) {
+		this.estoque = estoque;
+	}
 	
 	
 }
