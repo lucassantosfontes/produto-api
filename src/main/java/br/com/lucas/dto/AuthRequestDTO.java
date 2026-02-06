201babfba895a86fa183d6bde23184aeadf8f21c
lucassantosfontes <fonteslucasdossantos@gmail.com>
2026-02-06 15:44:33 -0300

    record AuthRequestDTO

---
+++ src/main/java/br/com/lucas/dto/AuthRequestDTO.java
@@ -0,0 +1,8 @@
+package br.com.lucas.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record AuthRequestDTO(
+        @NotBlank String username,
+        @NotBlank String password
+) {}
